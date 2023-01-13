0dcebc36797996fb2b24b08a0a1926afcd8b3595
pisdr <sq9wis@gmail.com>
2023-01-13 13:56:48 +0100

    Update Switch.ino

--- Switch.ino
+++ Switch.ino
@@ -1,11 +1,17 @@
 #include <Wire.h>  
 #include <LiquidCrystal_I2C.h>
 
-#define przekaz1 2  
-#define przekaz2 3
-#define przekaz3 4
+#define relay1 2  
+#define relay2 3
+#define relay3 4
 #define radio1 5
 #define radio2 6
+#define switchMode 7
+
+int switchStatus = HIGH;
+
+uint8_t band = 0;
+uint8_t lastBand = 0xff;
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4); 
 
@@ -20,9 +26,52 @@ byte antenna1[] = {
   B11110
 };
 
+static uint8_t bands[]={0, 160, 80, 40, 30, 20, 17, 15, 12, 10, 6, 0};
+
 void setup ()
 {
-  lcd.init();                    
+  startup();
+  
+  pinMode(relay1, OUTPUT); 
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(radio1, OUTPUT);
+  pinMode(radio2, OUTPUT);
+  
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, HIGH);
+  digitalWrite(relay3, HIGH);
+  digitalWrite(radio1, HIGH);
+  digitalWrite(radio2, HIGH);
+
+  pinMode(7, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(14, INPUT);
+  pinMode(15, INPUT);
+  pinMode(16, INPUT);
+  pinMode(17, INPUT);
+
+  lcd.createChar(1 , antenna1);
+}
+
+void loop()
+{
+  autoMode();
+  band = PINC & (0b00001111);
+  if (lastBand != band){
+  lcd.backlight();
+  lcd.setCursor(13, 0);
+  lcd.print(bands[band]);
+  lastBand = band;
+  } 
+ }
+
+ void startup()
+ { 
   lcd.init();
   lcd.backlight();
   lcd.setCursor(1,0);
@@ -42,35 +91,10 @@ void setup ()
   lcd.print("Radio -");
   lcd.setCursor(4, 1);
   lcd.print("B R A K");  
+ }
 
-  Serial.begin(9600);
-  
-  pinMode(przekaz1, OUTPUT); 
-  pinMode(przekaz2, OUTPUT);
-  pinMode(przekaz3, OUTPUT);
-  pinMode(radio1, OUTPUT);
-  pinMode(radio2, OUTPUT);
-  
-  digitalWrite(przekaz1, HIGH);
-  digitalWrite(przekaz2, HIGH);
-  digitalWrite(przekaz3, HIGH);
-  digitalWrite(radio1, HIGH);
-  digitalWrite(radio2, HIGH);
-
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-
-  lcd.createChar(1 , antenna1);
-
-
-}
-void loop()
-
-{
-
+ void manualMode()
+ {
   if (digitalRead(10) == LOW){ 
    digitalWrite(przekaz1, LOW);
    digitalWrite(przekaz2, HIGH);
@@ -109,7 +133,52 @@ void loop()
    lcd.setCursor(3,1);
    lcd.print("DIPOL 80M");
   }
- 
+ }
+
+ void autoMode()
+ {
+  if (bands[band] == 20){ 
+   digitalWrite(przekaz1, LOW);
+   digitalWrite(przekaz2, HIGH);
+   digitalWrite(przekaz3, HIGH); 
+   lcd.backlight();
+   lcd.setCursor(0,0);
+   lcd.write(1);
+   lcd.setCursor(1,0);
+   lcd.print("1");
+   lcd.setCursor(3,1);
+   lcd.print("DIPOL 20M");
+  } 
+  
+  if (bands[band] == 40){
+   digitalWrite(przekaz1, HIGH);
+   digitalWrite(przekaz2, LOW);
+   digitalWrite(przekaz3, HIGH);
+   lcd.backlight();
+   lcd.setCursor(0,0);
+   lcd.write(1);
+   lcd.setCursor(1,0);
+   lcd.print("2");
+   lcd.setCursor(3,1);
+   lcd.print("DIPOL 40M");
+  }
+  
+  if (bands[band] == 80){
+   digitalWrite(przekaz1, HIGH);
+   digitalWrite(przekaz2, HIGH);
+   digitalWrite(przekaz3, LOW);
+   lcd.backlight();
+   lcd.setCursor(0,0);
+   lcd.write(1);
+   lcd.setCursor(1,0);
+   lcd.print("3");
+   lcd.setCursor(3,1);
+   lcd.print("DIPOL 80M");
+  }
+ }
+
+ void radioSwitch()
+ {
   if (digitalRead(8) == LOW){
    digitalWrite(radio1, LOW);
    digitalWrite(radio2, HIGH);
@@ -118,6 +187,7 @@ void loop()
    lcd.print("Radio 1");
 
   }
+
   if (digitalRead(9) == LOW){
    digitalWrite(radio1, HIGH);
    digitalWrite(radio2, LOW);
@@ -125,5 +195,4 @@ void loop()
    lcd.setCursor(4,0);
    lcd.print("Radio 2");
   }
-  
  }
